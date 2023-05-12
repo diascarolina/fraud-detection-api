44baa93ba440810724f580112a2d3d9ec03246f1
Carolina Dias <carolina.dias@creditas.com>
2023-05-12 17:26:47 -0300

    feat: add upload endpoint

--- app/app.py
+++ app/app.py
@@ -1,14 +1,19 @@
 import os
+import io
 import uvicorn
 import pickle
 import logging
 import pandas as pd
-from fastapi import FastAPI, HTTPException, Request
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, HTTPException, Request, File, UploadFile
+from fastapi.responses import JSONResponse, StreamingResponse
 
 logger = logging.getLogger(__name__)
 
-app = FastAPI()
+app = FastAPI(
+    title="Fraud Detection API",
+    description="API for detecting credit card frauds",
+    version="1.0")
+
 
 model_path = os.path.join(os.path.dirname(__file__), 'modelo/modelo.pkl')
 model = pickle.load(open(model_path, 'rb'))
@@ -25,7 +30,8 @@ async def predict(request: Request):
         json_data = await request.json()
         data = pd.DataFrame(json_data, index=[0])
         preds = model.predict(data)[0]
-        return JSONResponse({"status": "success", "predicted_class": f"{preds}"})
+        return JSONResponse({"status": "success",
+                             "predicted_class": f"{preds}"})
     except Exception as e:
         logger.error(e)
         raise HTTPException(status_code=400, detail="Prediction failed.")
@@ -40,11 +46,25 @@ async def predict_batch(request: Request):
         json_data = await request.json()
         df = pd.DataFrame(json_data)
         df["preds"] = model.predict(df)
-        return JSONResponse({"status": "success", "predictions": f"{df['preds'].to_json(orient='records')}"})
+        return JSONResponse({"status": "success",
+                             "predictions": f"{df['preds'].to_json(orient='records')}"})
         
     except Exception as e:
         logger.error(e)
         raise HTTPException(status_code=400, detail="Batch prediction failed.")
+    
+
+@app.post("/upload_csv")
+def upload(file: bytes = File(...)):
+    df = pd.read_csv(io.BytesIO(file))
+    df["preds"] = model.predict(df)
+    stream = io.StringIO()
+    df.to_csv(stream, index=False)
+    response = StreamingResponse(
+        iter([stream.getvalue()]), media_type="text/csv")
+    response.headers["Content-Disposition"] = "attachment; filename=export.csv"
+    return response
+
 
 
 if __name__ == "__main__":
